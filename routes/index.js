c7eaf15731527410198395fddce1084c3ebab0cc
thanhtung6824@gmail.com <thanhtung6824@gmail.com>
2019-05-31 22:15:00 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('places/index', {
+router.get('/game', function (req, res, next) {
+    res.render('index', {
         layout: 'index',
         title: ':: Salefie App | Simplify sale field  ::',
         activity: 'Places',
